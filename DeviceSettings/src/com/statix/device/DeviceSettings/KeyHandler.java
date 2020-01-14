750ae92d04feb2e723ca388394c0fa8f76841333
rituj <ritujbeniwal@gmail.com>
2020-01-14 22:52:10 +0530

    sm8150-common: Improve Alert slider haptics
    Change-Id: I65da2a37ccd145dbb8f4ffb5de5bdb7360082066
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- DeviceSettings/src/com/statix/device/DeviceSettings/KeyHandler.java
+++ DeviceSettings/src/com/statix/device/DeviceSettings/KeyHandler.java
@@ -60,12 +60,13 @@ public class KeyHandler implements DeviceKeyHandler {
             return event;
         }
 
-        int scanCode = event.getScanCode();
-        int currentRingerMode = mAudioManager.getRingerModeInternal();
+        final int scanCode = event.getScanCode();
+        final int currentRingerMode = mAudioManager.getRingerModeInternal();
 
         switch (scanCode) {
             case MODE_NORMAL:
                 if (currentRingerMode != AudioManager.RINGER_MODE_NORMAL) {
+                    doHapticFeedback(VibrationEffect.EFFECT_DOUBLE_CLICK);
                     mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
                 } else {
                     return event;
@@ -73,6 +74,7 @@ public class KeyHandler implements DeviceKeyHandler {
                 break;
             case MODE_VIBRATION:
                 if (currentRingerMode != AudioManager.RINGER_MODE_VIBRATE) {
+                    doHapticFeedback(VibrationEffect.EFFECT_THUD);
                     mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE);
                 } else {
                     return event;
@@ -80,6 +82,7 @@ public class KeyHandler implements DeviceKeyHandler {
                 break;
             case MODE_SILENCE:
                 if (currentRingerMode != AudioManager.RINGER_MODE_SILENT) {
+                    doHapticFeedback(VibrationEffect.EFFECT_POP);
                     mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_SILENT);
                 } else {
                     return event;
@@ -92,9 +95,11 @@ public class KeyHandler implements DeviceKeyHandler {
         return null;
     }
 
-    private void doHapticFeedback(VibrationEffect effect) {
-        if (mVibrator != null && mVibrator.hasVibrator()) {
-            mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_POP));
+    private void doHapticFeedback(int effect) {
+        if (mVibrator != null) {
+            if (mVibrator.hasVibrator()) {
+                mVibrator.vibrate(VibrationEffect.get(effect));
+            }
         }
     }
 
