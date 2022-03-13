fedfb02a7729d4a91ebd93495d623f9ec5cf49fb
Cyber Knight <cyberknight755@gmail.com>
2022-03-14 02:04:36 +0800

    Revert "[DNM]: sm8150-common: Drop DeviceSettings"
    This reverts commit 541db8d0519d5f9e8007a135293732660e5c1fa5.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- common.mk
+++ common.mk
@@ -85,6 +85,10 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
+# Device Settings
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
