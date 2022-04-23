1161f1cfd9c31e5e2f76013fa12ee043b0625366
Cyber Knight <cyberknight755@gmail.com>
2022-04-24 02:24:38 +0800

    sm8150-common: Specify LLVM Tools support
    - To build with LLVM Binutils.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -45,6 +45,7 @@ TARGET_KERNEL_CLANG_COMPILE := false
 TARGET_KERNEL_NEW_GCC_COMPILE := true
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-elf-
 TARGET_KERNEL_CROSS_COMPILE_PREFIX_ARM32 := arm-eabi-
+KERNEL_SUPPORTS_LLVM_TOOLS := true
 KERNEL_TOOLCHAIN := $(PWD)/prebuilts/gcc/linux-x86/aarch64/aarch64-elf/bin
 TARGET_KERNEL_ADDITIONAL_FLAGS := "LD_LIBRARY_PATH=$(PWD)/prebuilts/gcc/linux-x86/aarch64/aarch64-elf/bin/ LD=aarch64-elf-ld.lld"
 TARGET_KERNEL_CONFIG := dragonheart_defconfig
