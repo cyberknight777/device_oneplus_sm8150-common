520fb3b947d50fe4ef1d637a134b9adffaa105d1
Cyber Knight <cyberknight755@gmail.com>
2022-11-26 10:42:21 +0530

    sm8150-common: Adapt to YAAP
    - aosp -> yaap.
    - overlay-aosp -> overlay-yaap.
    - DeviceSettings/src/com/aosp -> DeviceSettings/src/com/yaap.
    - SystemUIGoogle -> SystemUI.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- common.mk
+++ common.mk
@@ -23,7 +23,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-yaap
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += device/oneplus/sm8150-common/overlay
